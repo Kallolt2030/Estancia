2d629b4d2a75d614525308ef9e3302e42f46e4cb
Kallolt2030 <ktbernalgallegos@gmail.com>
2025-05-23 16:54:09 -0600

    sobre nosotros

--- public/nosotros.php
+++ public/nosotros.php
@@ -1,11 +1,13 @@
 <?php include '../includes/header.php'; ?>
 <link rel="stylesheet" href="../assets/css/nosotros.css">
+<!-- Añadir librería AOS para animaciones -->
+<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 
 <div class="video">
   <div class="overlay">
     <div class="contenedor contenido-video">
-      <h2>Estancia de Vida Nuestra Señora de Guadalupe</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit amet dolore harum ea minima debitis corrupti blanditiis placeat! Distinctio perspiciatis necessitatibus eum inventore voluptates pariatur nesciunt fuga eligendi dolore in?</p>
+      <h2 data-aos="fade-down" data-aos-duration="1000">Estancia de Vida Nuestra Señora de Guadalupe</h2>
+      <p data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit amet dolore harum ea minima debitis corrupti blanditiis placeat! Distinctio perspiciatis necessitatibus eum inventore voluptates pariatur nesciunt fuga eligendi dolore in?</p>
     </div>
   </div>
 
@@ -17,7 +19,7 @@
 <main>
   
 </main>
-<section class="quienes-somos contenedor">
+<section class="quienes-somos contenedor" data-aos="fade-up">
   <div class="quienes-somos__titulo">
     <h2>¿Quiénes Somos?</h2>
   </div>
@@ -33,11 +35,11 @@
 </section>
 
 <section class="historia contenedor">
-  <div class="historia__imagen">
-    <img src="../assets/img/historia.jpg" alt="Imagen de historia" width="100%">
+  <div class="historia__imagen" data-aos="fade-right">
+    <img src="../assets/img/historia.jpg" alt="Imagen de historia" class="imagen-con-sombra">
   </div>
 
-  <div class="historia__texto">
+  <div class="historia__texto" data-aos="fade-left">
     <h2>Nuestra Historia</h2>
     <p> 
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris condimentum maximus egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in purus metus. Cras ut urna nibh. Fusce porttitor nibh ligula, ac euismod metus tempor cursus. Phasellus molestie, justo sed egestas sodales, leo nisi luctus libero, eu rhoncus libero est ullamcorper justo. Quisque at urna non mi sollicitudin pretium non vel erat. Donec tincidunt velit dolor, vitae eleifend eros mollis mattis. Duis quis arcu vitae odio ultricies dictum.
@@ -48,8 +50,18 @@
   </div>
 </section>
 
-<div class="valores contenedor">
+<div class="valores contenedor" data-aos="zoom-in">
   <h2>Nuestros Valores</h2>
 </div>
+
+<!-- Inicializar AOS -->
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script>
+  AOS.init({
+    duration: 800,
+    easing: 'ease-in-out',
+    once: true
+  });
+</script>
 
 <?php include '../includes/footer.php'; ?>
