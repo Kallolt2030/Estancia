71492e5f53938fb210fd15630b5c5de392e83399
Kallolt2030 <ktbernalgallegos@gmail.com>
2025-06-26 13:36:36 -0600

    algo

--- dashboard/headerDash.php
+++ dashboard/headerDash.php
@@ -3,7 +3,7 @@
     <h1>Panel de usuario</h1>
     <nav>
       <ul>
-        <li><a href="index.php">Inicio</a></li>
+        <li><a href="index.php">Inicio</a></li> 
         <li><a href="cocina.php">Cocina</a></li>
         <li><a href="cuidadores.php">Cuidadores</a></li>
         <li><a href="expediente.php">Expedintes</a></li>
